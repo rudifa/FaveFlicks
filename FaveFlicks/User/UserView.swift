162cf1c84d3b369eba3ebe6a89a422cc5b4d79da
Rudi Farkas <rudi.farkas@gmail.com>
2021-10-28 22:04:18 +0200

    add code to struct UserView:

--- FaveFlicks/User/UserView.swift
+++ FaveFlicks/User/UserView.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2020 Razeware LLC
-/// 
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -33,9 +33,30 @@
 import SwiftUI
 
 struct UserView: View {
+  // 1
+  @State private var userName = "" // local storage
+
   var body: some View {
-    Text("Watch this space!")
+    NavigationView {
+      Form {
+        Section(header: Text("User")) {
+          // 2
+          TextField("User Name", text: $userName) // binding
+        }
+      }
+    }
+    // 3
+    .navigationBarTitle(Text("\(userName) Info"), displayMode: .inline)
+    .navigationBarItems(
+      trailing:
+      Button(action: updateUserInfo) {
+        Text("Update")
+      }
+    )
   }
+
+  // 4
+  func updateUserInfo() {}
 }
 
 struct UserView_Previews: PreviewProvider {
